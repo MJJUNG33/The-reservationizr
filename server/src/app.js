a9e7d5b0499f3c0c75c2e456455cfdee520d60b2
Minji Jung <minji.jung@developersinstitute.co.nz>
2023-06-01 19:49:18 +1200

    done initial setup to start project, GET /restaurnats and test for it on serverside

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,16 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const RestaurantsModel = require('./models/RestaurantModel');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/restaurants', async (req, res) => {
+  const restaurants = await RestaurantsModel.find({});
+  return res.status(200).send(restaurants);
+});
 
 module.exports = app;
--- server/src/models/ReservationModel.js
+++ server/src/models/ReservationModel.js
@@ -1,1 +1,13 @@
 // FIXME: Add a Mongoose model here
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const reservationSchema = new Schema({
+  partySize: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  userId: { type: String, required: true },
+  restaurantName: { type: String, required: true },
+});
+
+const Reservation = mongoose.model('reservation', reservationSchema);
+module.exports = Reservation;
