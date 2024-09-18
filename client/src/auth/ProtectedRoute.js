e2ece17fad65e5e9f8368827d0182c5a9cdeb708
Minji Jung <jung2674@gmail.com>
2024-09-18 21:36:44 +1200

    the reservationizr app

--- client/src/auth/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { Outlet } from "react-router-dom";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
-
-const ProtectedRoute = () => <Outlet />;
-
-export default withAuthenticationRequired(ProtectedRoute, {
-  onRedirecting: () => <p>Loading</p>,
-});
--- client/src/utils/formatDate.js
+++
@@ -1,5 +0,0 @@
-import { format } from "date-fns";
-
-const formatDate = (date) => format(new Date(date), "h:mmaaa EEE d LLL, yyyy");
-
-export { formatDate };
